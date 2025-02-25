3a79fc930902a6ebcb0ec216406410cb5e6e016a
arnau <arnau.sala01@estudiant.upf.edu>
2025-02-25 13:53:10 +0100

    semi4

--- SO/Seminari4/e1.c
+++ SO/Seminari4/e1.c
@@ -5,15 +5,16 @@
 #include <signal.h>
 #include <pthread.h>
 
-void thread_function(int i){
-    printf("soc el thread numero %d\n", i);
+void thread_function(void *i){
+    int i_int = (int)i;
+    printf("soc el thread numero %d\n", i_int);
     sleep(1);
 }
 
 int main(){
     pthread_t tid[100];
     for(int i = 0; i < 100; i++){
-        pthread_create(&tid[i], NULL, thread_function, &i);
+        pthread_create(&tid[i], NULL, thread_function, (void*)i);
     }
     for(int i = 0; i < 100; i++){
         pthread_join(&tid[i], NULL);
